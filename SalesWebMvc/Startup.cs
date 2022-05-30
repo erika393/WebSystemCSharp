24fb9ad50090966c85e39cdbbdf01e597dbf1e2d
erika.393 <erikaamericano393@gmail.com>
2022-05-30 00:03:23 -0300

    seeding service

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,16 +12,20 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 
-namespace SalesWebMvc {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace SalesWebMvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.Configure<CookiePolicyOptions>(options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
@@ -35,14 +39,25 @@ namespace SalesWebMvc {
             //e depois add um delegate e entre a segunda aspas é o nome do nosso projeto
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment()) {
+        //esse metodo permite que coloque outros parametros nele
+        //ao add o metodo seedingService no Configure automaticamente ja é instanciado ele mesmo
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
+        {
+            //se estivermos no perfil de desenvolvimento iremos rodar o SeedingService
+            if (env.IsDevelopment())
+            {
+
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
+                //entao populamos a base de dados caso ela nao esteja populada ainda
             }
-            else {
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
@@ -61,3 +76,5 @@ namespace SalesWebMvc {
         }
     }
 }
+
+
