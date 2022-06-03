2d902073b73c1221f48e484f93af584ad5612c56
Erika <erikaamericano393@gmail.com>
2022-06-02 23:08:05 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Data;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -43,6 +44,26 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {   
+            //o any serve pra falar se existe algum dado na condicao colocada
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found"); //excecao de nivel de servico
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message); //excecao de nivel de acesso a dados
+                //Aqui eh capturado a excecao em nivel de acesso a dados e relancada em nivel de servico
+            }
+            
         }
     }
 }
